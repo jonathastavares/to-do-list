3c05cdd253f8774938a1729a485fb653c730cce7
ixboy <ismaelixboy@gmail.com>
2021-10-07 05:47:47 +0100

    add positive test for saveItem method

--- src/list.saveItem.test.js
+++ src/list.saveItem.test.js
@@ -1,11 +1,29 @@
-jest.mock('./list')
-const List  = require('./list');
+import { saveItem } from './list.js';
 
-describe('Tests for List add function', () => {
+beforeAll(() => {
+  document.body.innerHTML = `
+    <div id='list-container' class='container w-25 shadow my-5'>
+    <ul class="list-group join-list-bottom no-round-borders">
+      <li class="list-group-item font-bold">Today's To Do List</li>
+      <li class='list-group-item d-flex'><input id='add-list' placeholder='Add to your list...'><button id='add-btn' type='submit' class='btn btn-secondary'>Add</button></li>
+    </ul>
+    <ul id='list-objects' class="list-group join-list-top join-list-bottom">
+      <!-- Add list elements Here -->
+    </ul>
+    <ul class='list-group join-list-top no-round-borders'>
+      <li class="list-group-item text-center"><button id='delete-completed-btn' type='submit' class='btn btn-danger w-100'>Clear all completed</button></li>
+    </ul>
+  </div>`;
+});
 
-    test('checks if a new item is being saved',() => {
-        const array = List.dataArray();
-        array = List.saveItem('test'));
-        expect(array[0].text).toBe('test');
-    });
+jest.mock('./__mocks__/localStorage.js');
+let listArray;
+
+describe('My To-Do List\'s adding functionality', () => {
+  test('Should add the new task into the list of to-do list', () => {
+    const newTodoInput = document.getElementById('add-list');
+    newTodoInput.value = 'New todolist!';
+    listArray = saveItem(newTodoInput.value);
+    expect(listArray).toHaveLength(1);
+  });
 });
