a72582032dc19dc8f4e6b60b3e29e04b4a25329d
Jonathas Tavares <jonathastavares@id.uff.br>
2021-10-07 14:02:12 -0300

    Edit positive test ready

--- src/list.editCompleteRemoveAll.test.js
+++ src/list.editCompleteRemoveAll.test.js
@@ -14,5 +14,16 @@ describe('My To-Do List\'s editing functionality', () => {
         listArray = List.updateItem('', 4);
         expect(listArray[3]).toMatchObject({text: 'New todolist!', completed: false, index: 4});
     });
+    
+    test('Should edit the new task into the list of to-do list', () => {
+        const newTodoInput = document.getElementById('add-list');
+        newTodoInput.value = 'New todolist!';
+        List.saveItem('Task 1');
+        List.saveItem('Task 2');
+        List.saveItem('Task 3');
+        List.saveItem(newTodoInput.value);
+        listArray = List.updateItem('Edit Test', 4);
+        expect(listArray[3]).toMatchObject({text: 'Edit Test', completed: false, index: 4});
+      });
   });
   
